b7fc34e0c12383ea7e2279ae5fda0ad18d69e3cc
priya-cardup <priya@cardup.co>
2021-07-06 11:41:12 +0530

    Users tab & devise - trackable

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
+  resources :users, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'home#index'
   # get 'static_pages/landing_page'
   # get 'static_pages/privacy_policy'
-  get 'privacy_policy', to: 'static_pages#privacy_policy'
+  # get 'privacy_policy', to: 'static_pages#privacy_policy'
 end

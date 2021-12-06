25ed9c2ab7201c66867be9b99d2b4f7920daddad
priya-cardup <priya@cardup.co>
2021-12-06 11:24:45 +0530

    lesson table, friendly_id, public_ativity

---
+++ app/models/lesson.rb
@@ -0,0 +1,16 @@
+class Lesson < ApplicationRecord
+  extend FriendlyId
+  include PublicActivity::Model
+
+  friendly_id :title, use: :slugged
+  tracked owner: Proc.new { |controller, model| controller.current_user }
+
+  belongs_to :course
+
+  validates_presence_of :title, :content, :course
+
+  def to_s
+    title
+  end
+
+end
